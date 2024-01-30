ecb74ceb5f6fb570c553b7273e3ce7ed2acf1629
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2024-01-30 18:35:43 +0000

    Lint fixes

--- code/mqtt.py
+++ code/mqtt.py
@@ -1,3 +1,4 @@
+"""Module to handle mqtt."""
 import json
 import random
 import string
@@ -5,13 +6,13 @@ import time
 import logging
 from dataclasses import dataclass
 
-import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt_client
 
 log = logging.getLogger()
 
 @dataclass
 class MqttConfig:
+    """Class to hold mqtt configs."""
     mqtt_server: str
     mqtt_port: int
     mqtt_client_id: str
@@ -29,6 +30,7 @@ class MqttConfig:
 
     @classmethod
     def from_dict(cls, config: dict):
+        """Create configs from dict."""
         return MqttConfig(
             mqtt_server = config.get("server", ""),
             mqtt_port = config.get("port", 1883),
@@ -39,14 +41,20 @@ class MqttConfig:
             mqtt_ha_enable = config.get("hass_enabled", True),
             lock_id = config.get("lock_id", "0"),
             mqtt_topic_prefix = config.get("prefix_topic", ""),
-            mqtt_ha_status_topic = config.get("hass_status_topic", "homeassistant/status"),
-            mqtt_oneline_topic = config.get("prefix_topic", "") + "/" + config.get("lock_id", "0") + "/online",
-            mqtt_state_topic = config.get("prefix_topic", "") + "/" + config.get("lock_id", "0") + "/mqtt_state_topic",
-            mqtt_command_topic = config.get("prefix_topic", "") + "/" + config.get("lock_id", "0") + "/command_topic",
-            mqtt_key_id_authed_topic = config.get("prefix_topic", "") + "/" + config.get("lock_id", "0") + "/key_id_authed"
+            mqtt_ha_status_topic = config.get(
+                "hass_status_topic", "homeassistant/status"),
+            mqtt_oneline_topic = config.get(
+                "prefix_topic", "") + "/" + config.get("lock_id", "0") + "/online",
+            mqtt_state_topic = config.get(
+                "prefix_topic", "") + "/" + config.get("lock_id", "0") + "/mqtt_state_topic",
+            mqtt_command_topic = config.get(
+                "prefix_topic", "") + "/" + config.get("lock_id", "0") + "/command_topic",
+            mqtt_key_id_authed_topic = config.get(
+                "prefix_topic", "") + "/" + config.get("lock_id", "0") + "/key_id_authed"
         )
 
 class Mqtt:
+    """Class to handle mqtt."""
     def __init__(
         self,
         config_dict:dict
@@ -56,16 +64,17 @@ class Mqtt:
         self.connect_mqtt()
 
     def connect_mqtt(self):
+        """Connect to mqtt."""
         log.info("Connecting to MQTT")
-        def on_connect(client, userdata, flags, rc):
+        def on_connect(client, _1, _2, rc):
             if rc == 0:
                 log.info("Connected to MQTT Broker!")
                 self.setup_subscriptions()
                 self.publish_hass_config()
-                self.client.publish(self.config.mqtt_oneline_topic, "online", 0, True)
+                client.publish(self.config.mqtt_oneline_topic, "online", 0, True)
             else:
                 log.error("Failed to connect, return code %d\n", rc)
-        def on_disconnect(client, userdata, rc):
+        def on_disconnect(_1, _2, _3):
             self.connect_mqtt()
             log.warning("Mqtt Disconnected")
         # Set Connecting Client ID
@@ -79,21 +88,25 @@ class Mqtt:
         self.client.connect(self.config.mqtt_server, self.config.mqtt_port)
         self.client.loop_start()
         log.info("MQTT Connection setup")
-    
+
     def setup_subscriptions(self):
-        def on_message(client, userdata, msg):
-            log.debug("Message recieved on topic: " + msg.topic + " With Message: " + msg.payload.decode())
+        """Setup mqtt subscriptions."""
+        def on_message(_1, _2, msg):
+            log.debug("Message recieved on topic: " +
+                      msg.topic + " With Message: " +
+                      msg.payload.decode())
             if msg.topic == self.config.mqtt_command_topic:
                 self.update_callback(True if msg.payload.decode() == "lock" else False)
             elif msg.topic == self.config.mqtt_ha_status_topic:
                 self.publish_hass_config()
-        
+
         self.client.subscribe(self.config.mqtt_command_topic)
         self.client.subscribe(self.config.mqtt_ha_status_topic)
         self.client.on_message = on_message
         log.info("Subscription setup")
-    
+
     def publish_hass_config(self):
+        """Publish hass config to mqtt."""
         time.sleep(10)
         hass_device_name = "mqtt-homekey-lock-" + self.config.lock_id
         hass_lock_name = "mqtt-homekey-lock-" + self.config.lock_id + "-lock"
@@ -129,20 +142,22 @@ class Mqtt:
 
     # This should be overriden by the accessory file
     def default_update_callback(self, set_locked:bool):
-        pass
+        """Create a default callback to call when mqtt sets state."""
 
     def update_state(self, target_locked:bool, current_locked:bool):
+        """Send status update to mqtt."""
         pub_state = "unkown"
-        if target_locked == False and current_locked == False:
+        if not target_locked and not current_locked:
             pub_state = "unlocked"
-        elif target_locked == True and current_locked == True:
+        elif target_locked and current_locked:
             pub_state = "locked"
-        elif target_locked == False and current_locked == True:
+        elif not target_locked and current_locked:
             pub_state = "unlocking"
-        elif target_locked == True and current_locked == False:
+        elif target_locked and not current_locked:
             pub_state = "locking"
         self.client.publish(self.config.mqtt_state_topic, pub_state, 0, True)
-    
+
     def device_passed_auth(self, key_id:str):
+        """Send device authentication method to mqtt."""
         self.client.publish(self.config.mqtt_key_id_authed_topic, key_id)
     
